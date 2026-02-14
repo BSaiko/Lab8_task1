2489d0c841755d4e41bd49b13b4891fdafafef7d
BSaiko <bogdan.suchenko2.0@gmail.com>
2026-02-14 04:17:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,21 @@ def get_dog_data():
     try:
         # 1. Відправляємо GET-запит на сервер 
         response = requests.get(url)
-
-        # Перевіряємо, чи успішний запит (код 200)
         response.raise_for_status()
 
-        # 2. Парсимо отриманий JSON в об'єкт Python 
         data = response.json()
 
-        # Отримуємо саме посилання на картинку
+        # Отримуємо саме посилання
         image_url = data['message']
 
         # 3. Витягуємо породу з URL
-        # Посилання виглядає так: https://images.dog.ceo/breeds/poodle-standard/n02113799_2280.jpg
-        # Розбиваємо текст по символу '/' і беремо передостанній елемент
         breed = image_url.split('/')[-2]
 
-        # 4. Виводимо результат в консоль, як просить завдання [cite: 526]
+        # 4. Виводимо результат в консоль, як просить завдання
         print("-" * 30)
-        print("🐶 ГЕНЕРАТОР СОБАК 🐶")
+        print(" ГЕНЕРАТОР СОБАК ")
         print("-" * 30)
-        print(f"Порода: {breed.upper()}") # робимо великими літерами для краси
-        print(f"URL фото: {image_url}")
+        print(f"Порода: {breed.upper()}") 
         print("-" * 30) 
 
     except requests.RequestException as e:
